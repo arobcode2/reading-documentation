b15de7e11fac2ac7628912320c287673e58c8363
Andrew Rob <andrew-c-rob@live.com>
2018-05-24 13:38:24 -0500

    Changed the implementation of the pie chart so that it shows the number of hours on the chart instead of the percentage

--- chart.js
+++ chart.js
@@ -10,6 +10,13 @@ var chart = c3.generate({
 			code: '#ef0e02',
 			other: '#12c105',
 			relax: '#f7f304' 
+		},
+	},
+	pie: {
+		label: {
+			format: function(value, ratio, id) {
+				return d3.format('')(value) + ' hours';
+			}
 		}
 	}
 });
